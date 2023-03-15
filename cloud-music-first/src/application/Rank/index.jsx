e36cc66546d70f4914c20f70681014640c6e08c5
zhoumowubuzuoye <www.1078637049@qq.com>
2023-03-15 17:04:27 +0800

    测试

--- cloud-music-first/src/application/Rank/index.jsx
+++ cloud-music-first/src/application/Rank/index.jsx
@@ -1,14 +1,14 @@
 /*
  * @Author: xiewenhao
  * @Date: 2023-03-14 18:01:47
- * @LastEditTime: 2023-03-14 18:05:38
+ * @LastEditTime: 2023-03-15 17:04:02
  * @Description:
  */
 
 import React from "react";
 
 function Rank(props) {
-  return <div>Rank</div>;
+  return <div>Rank1</div>;
 }
 
 export default React.memo(Rank);
