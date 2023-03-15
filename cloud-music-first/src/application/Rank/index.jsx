51a556345ef1555eabc94557aa61d35ba450a20a
zhoumowubuzuoye <www.1078637049@qq.com>
2023-03-15 17:26:48 +0800

    测试

--- cloud-music-first/src/application/Rank/index.jsx
+++ cloud-music-first/src/application/Rank/index.jsx
@@ -1,14 +1,14 @@
 /*
  * @Author: xiewenhao
  * @Date: 2023-03-14 18:01:47
- * @LastEditTime: 2023-03-15 17:19:51
+ * @LastEditTime: 2023-03-15 17:21:53
  * @Description:
  */
 
 import React from "react";
 
 function Rank(props) {
-  return <div>Rank1245</div>;
+  return <div>Rank124522</div>;
 }
 
 export default React.memo(Rank);
