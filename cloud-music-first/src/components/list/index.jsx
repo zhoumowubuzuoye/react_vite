249e494fe10af22a2dc8752e8b349297a31dc15c
zhoumowubuzuoye <www.1078637049@qq.com>
2023-03-15 16:57:02 +0800

    补交

--- cloud-music-first/src/components/list/index.jsx
+++ cloud-music-first/src/components/list/index.jsx
@@ -1,4 +1,11 @@
 /*
  * @Author: xiewenhao
  * @Date: 2023-03-15 16:54:45
- * @LastEditTime: 2023-03-15 16:56:0
+ * @LastEditTime: 2023-03-15 16:56:00
+ * @Description: 
+ */
+import React from "react";
+
+export default function List() {
+  return <div>List</div>;
+}
